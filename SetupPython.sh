1ecdc2d728ae02f9cd1acfe13551c47a746b578d
Joel Mousseau <joelam@fnal.gov>
2019-09-23 12:27:05 -0500

    Intial import

---
+++ SetupPython.sh
@@ -0,0 +1,4 @@
+export PYTHONUSERBASE=/uboone/app/users/wketchum/python_libs
+export PYTHONPATH=$PYTHONUSERBASE/bin:$PYTHONPATH
+export PATH=$PYTHONUSERBASE/bin:$PATH
+setup uboonecode v08_00_00_19 -q e17:prof
